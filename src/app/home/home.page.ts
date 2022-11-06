c2fc4e0fbee49064912ef291d2c81aa6eb58c0a0
Hamedkabir <38732428+Hamedkabir@users.noreply.github.com>
2022-11-06 14:48:44 +0100

    image compress

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import {
+  DataUrl,
+  NgxImageCompressService,
+  UploadResponse,
+} from 'ngx-image-compress';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +11,77 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  imgResultBeforeCompress: DataUrl = '';
+  imgResultAfterCompress: DataUrl = '';
+  ratio: any;
+  quality: number;
+  orientation: any;
+  constructor(private imageCompress: NgxImageCompressService) {}
+  rangeChange(event: any, name: string) {
+    switch (name) {
+      case 'ratio':
+        this.ratio = event;
+        break;
 
-  constructor() {}
+      case 'quality':
+        this.quality = event;
+        break;
+      default:
+        break;
+    }
+    this.compressFile(
+      this.imgResultBeforeCompress,
+      this.orientation,
+      this.ratio,
+      this.quality
+    );
+  }
 
+  async uploadFile() {
+    const { image, orientation, fileName } =
+      await this.imageCompress.uploadFile();
+    this.imgResultBeforeCompress = image;
+    this.orientation = orientation;
+    console.log('File Name:', fileName);
+    console.log(
+      `Original: ${image.substring(0, 50)}... (${image.length} characters)`
+    );
+    console.log('Size in bytes was:', this.imageCompress.byteCount(image));
+    if (image) {
+      this.compressFile(
+        this.imgResultBeforeCompress,
+        this.orientation,
+        undefined,
+        undefined
+      );
+    }
+  }
+  compressFile(
+    image?: string,
+    orientation?: any,
+    ratio?: number,
+    quality?: number
+  ) {
+    this.imageCompress
+      .compressFile(
+        image,
+        orientation,
+        ratio ? ratio : 100,
+        quality ? quality : 50
+      )
+      .then((result_1: DataUrl) => {
+        this.imgResultAfterCompress = result_1;
+        console.log(
+          `Compressed: ${result_1.substring(0, 50)}... (${
+            result_1.length
+          } characters)`
+        );
+        console.log(
+          'Size in bytes is now:',
+          this.imageCompress.byteCount(result_1)
+        );
+        console.log('ratio is ', ratio);
+        console.log('quality  ', quality);
+      });
+  }
 }
